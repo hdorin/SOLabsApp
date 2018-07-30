cd1ea379d6ef3a17bb9534db76a7f42816c8e150
hdorin <halocadorin@outlook.com>
2018-07-30 11:56:52 +0300

    Phpmyadmin merge!

--- mvc/app/models/SSHConnection.php
+++ mvc/app/models/SSHConnection.php
@@ -4,19 +4,14 @@ class SSHConnection
 {
     private $user,$pass;
     public function create_new_user(){
-        
         if (!($connection = ssh2_connect('127.0.0.1', '22'))) {
             throw new Exception('Could not establish SSH connection!');
         } 
-        
         if (!ssh2_auth_password($connection, 'dorin', 'halogenuri')) {
             throw new Exception('Could not access administrator account!');
         }
-        
         ssh2_exec($connection, 'sudo ./CreateUser.sh ' . $this->pass . ' '. $this->user);
-
         unset($connection);
-        
     }
     public function connect($user,$pass,$first_login){
         $this->user=$user;
@@ -26,19 +21,20 @@ class SSHConnection
                 throw new Exception('Could not establish external SSH connection!');
             }
                 if (!ssh2_auth_password($external_cconnection, $this->user, $this->pass)) {
+                    unset($external_cconnection);
                     return false;
                 }
             unset($external_cconnection);  
             $this->create_new_user();
-            //sleep(2);
         }
         if (!($connection = ssh2_connect('127.0.0.1', '22'))) {
             throw new Exception('Could not establish SSH connection!');
-        } 
-    
+        }
         if (ssh2_auth_password($connection, $this->user, $this->pass)) {
+            unset($connection);
             return true;
         }else{
+            unset($connection);
             return false;
         }
     }
