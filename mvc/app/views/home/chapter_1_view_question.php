545882c831cfee552bedcdd01bf62e9f0519a39d
Dorin <halocadorin@outlook.com>
2018-10-02 20:54:10 +0300

    Din nou progrese. Sa nu uit de butoane pentru pagini

--- mvc/app/views/home/chapter_1_view_question.php
+++ mvc/app/views/home/chapter_1_view_question.php
@@ -10,7 +10,23 @@
     <?php
         include "header.php"
     ?>
+    <div class='questionBox' >
+        <p class='questionText'><?php echo  $data["question_text"] ;?> </p>
+        <div class='questionDetailsBox'>
+            <p class='questionDetails'><?php echo  "Times answered: " . $data["right_answers"] . " / " . $data["all_answers"] ;?> </p>
+            <p class='questionDetails'><?php echo "Validation: " . $data["validation"]; ?> </p>
+            <p class='questionDetails'><?php echo "Date submitted: " . $data["date_submitted"]; ?> </p>
+        </div>
     
+        <form class='deleteQuestion'>
+            <input class="deleteBtn" type="submit" value="Delete" />
+        </form>
+        <h2>Reports</h2>
+        <div class='reportsBox'>
+            <p class='reportText'><?php echo  $data["question_text"] ;?> </p>
+            <p class='reportDetails'><?php echo  "Date submitted: " . $data["right_answers"] . " / " . $data["all_answers"] ;?> </p>
+        </div>
+    </div>
 </body>
 </html>
 
