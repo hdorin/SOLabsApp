1402ebe9d04ef37331dda946414a418187505985
Dorin <halocadorin@outlook.com>
2018-12-30 22:11:07 +0200

    Terminat adaugat campuri de cautare la All Questions

--- mvc/app/controllers/view_questions.php
+++ mvc/app/controllers/view_questions.php
@@ -1,8 +1,8 @@
 <?php
 class View_Questions extends Controller
 {
-    private $questions;
-    private $questions_nr;
+    private $questions,$questions_nr;
+    private $chapters,$chapters_nr;
     public function index()
     {
         $this->check_login();
@@ -13,13 +13,60 @@ class View_Questions extends Controller
 
         
         $this->get_questions();
-        $this->view('home/questions',['questions' => $this->questions,'questions_nr' => $this->questions_nr]);
+        $this->get_chapters();
+        $this->view('home/questions',['questions' => $this->questions,'questions_nr' => $this->questions_nr,'chapters' => $this->chapters,'chapters_nr'=>$this->chapters_nr]);
     }
-    
+    public function refresh_criteria(){
+        $this->check_login();
+        if($this->session_is_admin==false){
+            die("You cannot access this!");
+        }
+        $config=$this->model('JSONConfig');
+        $db_host=$config->get('db','host');
+        $db_user=$config->get('db','user');
+        $db_pass=$config->get('db','pass');
+        $db_name=$config->get('db','name');
+        $ssh_connection=$this->model('SSHConnection');
+        $db_connection=$this->model('DBConnection');
+        $link=$db_connection->connect($db_host,$db_user,$db_pass,$db_name);
+        if(strcmp($_POST["status_field"],"posted")==0){
+            $_SESSION["criteria_posted"]="AND q.`status`='posted'";
+        }else{
+            $_SESSION["criteria_posted"]="AND q.`status`='deleted'";
+        }
+        if(!empty($_POST["user_field"])){
+           
+            $sql=$link->prepare('SELECT id FROM users WHERE `user_name`=?');
+            $sql->bind_param('s', $_POST["user_field"]);
+            $sql->execute();
+            $sql->bind_result($aux_user_id);
+            if(!$sql->fetch()){
+                $aux_user_id="-1";
+            }
+            $sql->close();
+            $_SESSION["criteria_user"]="AND q.`user_id`=" . $aux_user_id;
+        }else{
+            $_SESSION["criteria_user"]=" ";
+        }
+        if(empty($_POST["chapter_field"]) || strcmp($_POST["chapter_field"],"all")==0){
+            $_SESSION["criteria_chapter"]=" ";
+        }else{
+            $_SESSION["criteria_chapter"]="AND q.chapter_id=" . $_POST["chapter_field"];
+           
+        }
+        $db_connection->close();
+        $this->reload();
+    }
+    private function reload(){
+        $new_url="../view_questions";
+        header('Location: '.$new_url);
+        die;
+    }
     private function get_questions(){
-        $is_posted=" ";
-        $search_user=" ";
-        
+        $question_posted=$_SESSION["criteria_posted"];
+        $search_user=$_SESSION["criteria_user"];
+        $search_chapter=$_SESSION["criteria_chapter"];
+
         $config=$this->model('JSONConfig');
         $db_host=$config->get('db','host');
         $db_user=$config->get('db','user');
@@ -31,13 +78,11 @@ class View_Questions extends Controller
         if($this->session_is_admin==false){
             $is_posted="AND q.`status`='posted'";
             $search_user="AND q.`user_id`=" . (string)$this->session_user_id;
+            $search_chapter=" ";
         }
-            $qurery="SELECT q.id,q.`chapter_id`,q.all_answers,q.right_answers,q.`validation`,c.name,q.date_created FROM questions q JOIN chapters c ON q.chapter_id=c.id WHERE 1=1 " . $search_user .  " " . $is_posted . " ";    
+            $qurery="SELECT q.id,q.`chapter_id`,q.all_answers,q.right_answers,q.`validation`,c.name,q.date_created FROM questions q JOIN chapters c ON q.chapter_id=c.id WHERE c.status='posted' " . $search_user .  " " . $question_posted . " " . $search_chapter;    
         
         $sql=$link->prepare($qurery);
-        /*if($this->session_is_admin==false){
-            $sql->bind_param('i', $this->session_user_id);
-        }*/
         $sql->execute();
         $sql->bind_result($question_id,$chapter_id,$all_answers,$right_answers,$validation,$chapter_name, $date_submitted);
         $this->questions_nr=0;
@@ -64,6 +109,33 @@ class View_Questions extends Controller
                                                             </a>";
             }
             $this->questions_nr=$this->questions_nr+1;
+        }
+        $sql->close();
+        $db_connection->close();
+    }
+    private function get_chapters(){
+        if($this->session_is_admin==false){
+            $this->chapters="";
+            $this->chapters_nr=0;
+            return 0;
+        }
+        $config=$this->model('JSONConfig');
+        $db_host=$config->get('db','host');
+        $db_user=$config->get('db','user');
+        $db_pass=$config->get('db','pass');
+        $db_name=$config->get('db','name');
+        $ssh_connection=$this->model('SSHConnection');
+        $db_connection=$this->model('DBConnection');
+        $link=$db_connection->connect($db_host,$db_user,$db_pass,$db_name);
+        
+        $sql=$link->prepare("SELECT id,`name` FROM chapters WHERE `status`='posted'");
+        $sql->execute();
+        $sql->bind_result($chapter_id,$chapter_name);
+        $this->chapters_nr=1;
+        $this->chapters[0]='<option value="all">All</option>';
+        while($sql->fetch()){
+                $this->chapters[$this->chapters_nr]='<option value="' . (string)$chapter_id . '">' . $chapter_name . '</option>';
+            $this->chapters_nr=$this->chapters_nr+1;
         }
         $sql->close();
         $db_connection->close();
--- mvc/app/views/home/questions.php
+++ mvc/app/views/home/questions.php
@@ -14,15 +14,35 @@
     include "header.php"
 ?>
     <!--Restrict this form only to admins-->
-    <form class="questionsCriteria" action="questions/get_questions" method="POST">
-        <h3>User</h3>
-        <input class="userField" name="user_field" type="text">
-        <select>
-            <option value="posted">Posted</option>
-            <option value="deleted">Deleted</option>
-        </select>
-        <input class="refreshButton" type="submit" value="Refresh" />
-    </form>
+    <?php 
+        if($_SESSION['is_admin']==true){
+            echo '
+                <form class="questionsCriteria" action="view_questions/refresh_criteria" method="POST">
+                    <h3>User</h3>
+                    <input class="userField" name="user_field" type="text">
+                    <h3>Status</h3>
+                    <select name="status_field">
+                        <option value="posted">Posted</option>
+                        <option value="deleted">Deleted</option>
+                    </select>
+                    <select name="chapter_field">';
+        }
+    ?>
+            <?php 
+                if($_SESSION['is_admin']==true){
+                    for($i=0;$i<$data['chapters_nr'];$i++){
+                        echo $data['chapters'][$i];
+                    }
+                }
+            ?>
+    <?php 
+        if($_SESSION['is_admin']==true){
+            echo '
+                    </select>
+                    <input class="refreshButton" type="submit" value="Refresh" />
+                </form>';
+        }
+    ?>
     <div class="questionsBox">
         <?php 
             for($i=0;$i<$data['questions_nr'];$i++){
